cfe12688d75bd6af417734e75f471ce43b1ab95f
sqrlfirst <44490789+sqrlfirst@users.noreply.github.com>
2019-05-23 19:16:40 +0700

    Delete modelsim testfiles

--- metoda/@t_trigger/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity T_trigger is
-    port(
-        T               : in     vl_logic;
-        clk             : in     vl_logic;
-        q               : out    vl_logic
-    );
-end T_trigger;
--- metoda/triggers_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity triggers_tb is
-end triggers_tb;
